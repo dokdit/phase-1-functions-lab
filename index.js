c340eec4dbcd5810164a8c039164e7e3b1d4951f
AK <akueiminyang@cmail.carleton.ca>
2023-03-22 03:58:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street-42)
+}
+
+function distanceFromHqInFeet(street){
+    return 264 * distanceFromHqInBlocks(street)
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start-destination) * 264
+}
+
+function calculatesFarePrice(start,destination){
+    let farePrice = 0
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400){
+        farePrice = 0
+    }else if (distance>400 && distance <= 2000){
+        farePrice = (2*(distance-400))/100
+    }else if(distance>2000 && distance<=2500){
+        farePrice = 25
+    }else{
+        farePrice = "cannot travel that far"
+    }
+    return farePrice
+}
